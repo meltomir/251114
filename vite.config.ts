b3f362022a4c3ddb180b9e9699cd2e8888449f6a
meltomir <douxylo@gmail.com>
2025-11-15 01:08:10 +0900

    chore: add pages workflow & set Vite base

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
+  base: '/251114/',
   plugins: [
     react({
       babel: {
